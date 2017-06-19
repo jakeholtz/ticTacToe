eb271d4ba00e14c963104403f619577cceacf246
Jake Holtz <jahholtz@gmail.com>
2017-06-19 14:50:32 -0700

    Added basic ui for command line

---
+++ ticTacToe.js
@@ -0,0 +1,43 @@
+// TIC TAC TOE jakeholtz 6/19/2017
+
+var title = ( 'TIC TAC TOE')
+var players = ['person1', 'person2'];
+var turn = '(Turn: ' + players[0] + ')';
+
+var line = (' _____');
+var top = ('|_|_|_|');
+var mid = ('|_|_|_|');
+var bot = ('|_|_|_|');
+
+console.log(title);
+console.log(turn);
+console.log(line);
+console.log(top);
+console.log(mid);
+console.log(bot);
+
+//  _____
+// |_|_|_|
+// |_|_|_|
+// |_|_|_|
+
+
+//GOALS
+// that redraws the board
+// makes clear whose turn it is, each turn.
+// Players can submit moves
+   // (assume, admittedly unrealistically that both players are
+   // sitting at the same keyboard).
+// Win detection - detect and display who won
+
+// Implementation instructions
+//    @@@@@@@@ Create the project from scratch. Don't just clone an existing project.
+  //  @@@@@@@@ configuration files for any dependencies and test framework setup.
+// * You should have a reasonably thorough suite of
+  // unit tests using a real unit test framework.
+// * Use the editor of your choice.
+// * @@@@@@@@Init a git repo for this project.
+// * @@@@@@@@ Push the repo up to github.
+// * Make small commits as you go to illustrate your thought process and be able to back out changes easily.
+// * Don't forget to push your final solution up to Github.
+// * Add a professional-looking README file with installation and usage instructions.
